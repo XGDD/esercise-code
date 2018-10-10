cd13a484d75f11d73426076ee1c26a3e335c96e5
XGDD <42710088+XGDD@users.noreply.github.com>
2018-10-10 20:49:45 +0800

    不同数据类型的数值范围

---
+++ DataRange.java
@@ -0,0 +1,10 @@
+public class DataRange{
+    public static void main(String[] args){
+        System.out.println("Short \t 数值范围："+ Short.MAX_VALUE+": "+ Short.MIN_VALUE);
+        System.out.println("Integer \t 数值范围："+ Integer.MAX_VALUE+": "+ Integer.MIN_VALUE);
+        System.out.println("Long \t 数值范围："+ Long.MAX_VALUE+": "+ Long.MIN_VALUE);
+        System.out.println("Byte \t 数值范围："+ Byte.MAX_VALUE+":"+ Byte.MIN_VALUE);
+        System.out.println("Float \t 数值范围："+ Float.MAX_VALUE+":"+ Float.MIN_VALUE);
+        System.out.println("Double \t 数值范围："+ Double.MAX_VALUE+":"+ Double.MIN_VALUE);
+    }
+}
