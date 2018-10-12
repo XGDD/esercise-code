752ed06d18f169cc0b7e2f6cd63119f009e9e0e1
XGDD <42710088+XGDD@users.noreply.github.com>
2018-10-12 19:45:02 +0800

    VariableDemo.java

---
+++ VariableDemo.java
@@ -0,0 +1,9 @@
+public class VariableDemo{
+    public static void main(String[] args){
+        int ageOfStudent = 5;
+        double scoreOfStudent = 80.0;
+        char levelOfStudent = 'B';
+        System.out.println("年纪\t 得分\t 等级");
+        System.out.println(ageOfStudent+"\t"+scoreOfStudent+"\t"+levelOfStudent+"\t");
+    }
+}
