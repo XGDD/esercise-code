a7f4a495a63bcaa6849ff1bba9661a92cd4b1861
XGDD <42710088+XGDD@users.noreply.github.com>
2018-10-14 18:35:45 +0800

    MenuDemo.java

---
+++ MenuDemo.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class MenuDemo{
+    public static void main(String[] args){
+        int choice;
+        Scanner scanner = new Scanner(System.in);
+        do{
+        System.out.println("Help on: ");
+        System.out.println("1. if");
+        System.out.println("2. switch");
+        System.out.println("3. while");
+        System.out.println("4. do-while");
+        System.out.println("5. for\n");
+        System.out.println("Choose one: ");
+        choice = scanner.nextInt();
+        }while(choice < '1' || choice > '5');
+        System.out.println("\n");
+        switch(choice){
+        case 1:
+        System.out.println("The if: \n");
+        System.out.println("if(condition) statement; ");
+        System.out.println("else statement");
+        break;
+        case 2:
+        System.out.println("The switch: \n");
+        System.out.println("switch(expersion) {");
+        System.out.println("case constant: ");
+        System.out.println("statement sequence");
+        System.out.println("break; ");
+        System.out.println("//...");
+        System.out.println("}");
+        break;
+        case 3:
+        System.out.println("The while: \n");
+        System.out.println("while(condition)statement: ");
+        break;
+        case 4:
+        System.out.println("The do-while: \n");
+        System.out.println("do {");
+        System.out.println("statement; ");
+        System.out.println("} while(condtion); ");
+        break;
+        case 5:
+        System.out.println("The for: \n");
+        System.out.println("for(init; condition; iteration");
+        System.out.println("statement; ");
+        break;
+        }   
+    }
+}
