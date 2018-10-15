5e14c06649891f50f8324f4152b2a0af80db3073
XGDD <42710088+XGDD@users.noreply.github.com>
2018-10-15 11:23:34 +0800

    观察成员变量的初始值.java
    创建一个Test类，包含有一个public权限的int型成员变量与一个char类型的成员变量，观察在main方法中的初始值

---
+++ ObjectiveInit.java
@@ -0,0 +1,12 @@
+class Test{
+    public int a;
+    public char c;
+}
+
+public class ObjectiveInit{
+    public static void main(String[] args){
+        Test p = new Test();
+        System.out.println(p.a);
+        System.out.println(p.c);
+    }
+}
