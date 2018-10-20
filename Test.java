d243db593a24ab0d1f8e4554993758f8e952dd11
XGDD <42710088+XGDD@users.noreply.github.com>
2018-10-20 09:28:30 +0800

    运行时多态.java

--- Test.java
+++ Test.java
@@ -1,8 +1,29 @@
-//主类
-public class Test{
-    public static void main(String[] args){
-        System.out.println("你好 世界");
-       // System.out.print(args[1]);
+class superA{
+    int i = 100;
+    void fun(){
+        System.out.println("This is superA");
     }
 }
-
+class subB extends superA{
+    int m = 1;
+    void fun(){
+        System.out.println("This is subB");
+    }
+}
+class subC extends superA{
+    int n = 1;
+    void fun(){
+        System.out.println("This is subC");
+    }
+}
+class Test{
+    public static void main(String[] args){
+        superA a;
+        subB b = new subB();
+        subC c = new subC();
+        a = b;
+        a.fun();
+        a = c;
+        a.fun();
+    }
+}
