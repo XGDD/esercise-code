ba3dca842edf856e789d5792fafa86ae6971fc35
XGDD <42710088+XGDD@users.noreply.github.com>
2018-10-22 08:29:34 +0800

    Create 静态方法.java

---
+++ 静态方法.java
@@ -0,0 +1,10 @@
+class Simple{
+    static void go(){
+        System.out.println("Go...");
+    }
+}
+public class Constants{
+    public static void main(String[] args){
+        Simple.go();
+    }
+}
