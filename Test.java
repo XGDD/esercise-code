fb76b77e0765e14b67c012ef6e24fe872bcc2abf
XGDD <42710088+XGDD@users.noreply.github.com>
2018-10-24 09:31:34 +0800

    模板方法.java

--- Test.java
+++ Test.java
@@ -1,14 +1,40 @@
-class Father{
-    private String name = "zhangjun";
-    class Child{
-        void introFather(){
-            System.out.println(name);
-        }
+abstract class CaffeineBeverage{
+    final void prepareRecipe(){
+        boilwater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+    abstract void brew();
+    abstract void addCondiments();
+    void boilwater(){
+        System.out.println("Boiling Water");
+    }
+    void pourInCup(){
+        System.out.println("Pouring into cup");
+    }
+}
+class Tea extends CaffeineBeverage{
+    void brew(){
+        System.out.println("Steepint the tea");
+    }
+    void addCondiments(){
+        System.out.println("Adding Lemon");
+    }
+}
+class Coffee extends CaffeineBeverage{
+    void brew(){
+        System.out.println("Dripping Coffee through filter");
+    }
+    void addCondiments(){
+        System.out.println("Adding Sugar and Milk");
     }
 }
 public class Test{
     public static void main(String[] args){
-        Father.Child in = new Father().new Child();
-        in.introFather();
+        CaffeineBeverage coffee = new Coffee();
+        coffee.prepareRecipe();
+        CaffeineBeverage tea = new Tea();
+        tea.prepareRecipe();
     }
 }
